247fe54f797b0f9d364b64de0b1ff07df1527511
Daria <dar.zavaliuk@gmail.com>
2022-07-04 20:38:56 +0300

    Types

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function toReadable (number) {
+var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+module.exports = function toReadable(number) {
   
 }
